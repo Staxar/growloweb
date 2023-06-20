91adbb3052bc8a1cc2877a17c075f28921be90f2
madomaga <marcin.domagala@capgemini.com>
2023-06-20 18:19:23 +0200

    header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -3,7 +3,7 @@ export const Header = () => {
   return (
     <section className="header">
       <div className="header__container flex flex__column">
-        <header className="flex">
+        <header className="flex header__top">
           <div>
             <h1 className="header__logo">Growlo</h1>
           </div>
@@ -14,10 +14,8 @@ export const Header = () => {
             <span>Categories</span>
           </div>
         </header>
-        <div className="flex">
-          <h1>
-            Start to use Growlo with reliable farmers with reliable farmers
-          </h1>
+        <div className="flex header__content">
+          <h1>Stop wasting food and start making money</h1>
         </div>
       </div>
     </section>
